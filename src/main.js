ef5c0070b5941669cae9a217f030aef1a7b8900a
Alecell <alecell.dev@gmail.com>
2023-12-01 23:03:38 -0300

    chore: add commit lint, commitizien, husky, vitest and make build work

--- src/main.js
+++
@@ -1,8 +0,0 @@
-((PET) => {
-  function onDOMContentLoaded() {
-    const countersToInit = PET.Counter.getCountersOnPage();
-    countersToInit.forEach(PET.Counter.initCounter);
-  }
-
-  document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
-})((window.PET = window.PET || {}));
