69453b32b9d7dad8b2c2e1a34847958778aaa582
Diogo Reis <diogo.caronte@gmail.com>
2023-12-06 00:00:52 -0300

    add more tests

--- src/utils/id/index.spec.js
+++ src/utils/id/index.spec.js
@@ -2,27 +2,33 @@ import { describe, expect, it } from 'vitest';
 import createIDFactory from '.';
 
 describe('id', () => {
-  it('should generators with different prefixes starts in 1', () => {
-    const prefix1 = 'prefix-one';
-    const prefix2 = 'prefix-two';
+  describe('when create generators with different prefixes', () => {
+    it('each factory starts in 1', () => {
+      const prefix1 = 'prefix-one';
+      const prefix2 = 'prefix-two';
 
-    const generator1 = createIDFactory(prefix1);
-    const generator2 = createIDFactory(prefix2);
+      const generator1 = createIDFactory(prefix1);
+      const generator2 = createIDFactory(prefix2);
 
-    expect(generator1).not.toBe(generator2);
+      expect(generator1).not.toBe(generator2);
 
-    expect(generator1()).toBe(`${prefix1}-1`);
-    expect(generator1()).toBe(`${prefix1}-2`);
+      expect(generator1()).toBe(`${prefix1}-1`);
+      expect(generator1()).toBe(`${prefix1}-2`);
 
-    expect(generator2()).toBe(`${prefix2}-1`);
-    expect(generator2()).toBe(`${prefix2}-2`);
+      expect(generator2()).toBe(`${prefix2}-1`);
+      expect(generator2()).toBe(`${prefix2}-2`);
+
+      expect(generator1).not.toBe(generator2);
+    });
   });
 
-  it('should return the same instance for the same prefix', () => {
-    const prefix = 'sameInstance';
-    const generator1 = createIDFactory(prefix);
-    const generator2 = createIDFactory(prefix);
+  describe('when create generators with the same prefix', () => {
+    it('returns the same generator instance', () => {
+      const prefix = 'sameInstance';
+      const generator1 = createIDFactory(prefix);
+      const generator2 = createIDFactory(prefix);
 
-    expect(generator1).toBe(generator2);
+      expect(generator1).toBe(generator2);
+    });
   });
 });
