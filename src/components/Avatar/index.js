4009756c39acc7727c55fe75ab8ba67953ab1ab2
Carlos Bryan <carlosbryanpv@gmail.com>
2024-01-09 16:02:25 -0300

    feat: avatar button issue #18

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,23 @@
+import { Component } from 'pet-dex-utilities';
+import './index.scss';
+import icon from './Icon.svg';
+import iconHover from './iconHover.svg';
+
+const html = `
+  <a href="#" class="avatarButton" data-select="container">
+    <img class="plus" data-select="image" src="${icon}" alt="" />
+  </a>
+`;
+
+export default function AvatarButton() {
+  Component.call(this, { html });
+
+  const $image = this.selected.get('image');
+  const imageOriginal = icon;
+  const imageHover = iconHover;
+
+  $image.addEventListener('mouseover', () => ($image.src = imageHover));
+  $image.addEventListener('mouseout', () => ($image.src = imageOriginal));
+}
+
+AvatarButton.prototype = Object.assign(AvatarButton.prototype, Component.prototype);
--- src/home/main.js
+++ src/home/main.js
@@ -1,30 +1,11 @@
 import { extractElements } from 'pet-dex-utilities';
-import Card from '../components/Card';
 import './index.scss';
-
-function renderCards(qty, $container) {
-  for (let i = 0; i < qty; i += 1) {
-    const card = new Card();
-    card.mount($container);
-    card.setTitle(`Card ${i}`);
-    card.listen('purchase', () => {
-      console.log(`purchase de quem usa o componente, esse é o card ${i}`);
-    });
-
-    const $card = card.selected.get('card-button');
-
-    if (i === 2) {
-      card.disable();
-      $card.classList.add('card-container__button--disabled');
-    } else {
-      $card.classList.add('card-container__button--active');
-    }
-  }
-}
+import AvatarButton from '../components/Avatar';
 
 document.addEventListener('DOMContentLoaded', () => {
   const selected = extractElements([document.body]);
   const $container = selected.get('container');
-
-  renderCards(5, $container);
+  const avatar = new AvatarButton();
+  avatar.mount($container);
 });
+
