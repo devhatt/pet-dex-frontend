9f02053e4e37d2666a5855a9891bdafe3afdf5b5
Anthony <anthonymanoel12@gmail.com>
2024-01-10 14:14:43 -0300

    adding slider-input-range component

---
+++ src/components/PetRegist/index.js
@@ -0,0 +1,50 @@
+import { Component } from 'pet-dex-utilities';
+import './index.scss';
+import Bars from './Bars.svg';
+
+const html = `
+  <section class="slider">
+    <div class="slider__text-slider">
+        <h3 data-select="weight" class="slider__weight">0</h3>
+    </div>
+    <div class="slider__container">
+      <input type="range" value="50" step="0.1" min="0" max="100" data-select="rangeSlider" class="slider__input"/>
+      <img class="slider__bars-svg" alt="rangeSlider" src="${Bars}" data-select="svg">
+    </div>    
+  </section>
+`;
+
+const events = ['weight'];
+
+export default function Slider() {
+  Component.call(this, { html, events });
+  this.enable = true;
+
+  this.selected.get('rangeSlider').addEventListener(
+    'input',
+    (event) => {
+      if (!this.enable) return;
+      this.weight();
+      this.setValue(event.target.value);
+    },
+    false,
+  );
+}
+Slider.prototype = Object.assign(Slider.prototype, Component.prototype, {
+  setTitle() {
+    this.selected.get('weight').textContent = Text;
+  },
+  setValue() {
+    this.selected.get('weight').textContent =
+      this.selected.get('rangeSlider').value;
+  },
+  weight() {
+    this.emit('weight');
+  },
+  disable() {
+    this.enable = false;
+  },
+});
+
+// always start w/ const if will need changes, u made it.
+// forget .innerHTML uses textContent or innerText
