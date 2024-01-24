6d272b0ed8f57b2292eeeb9152cedc3784c16eac
nathalia <nathaliaazmelo@gmail.com>
2024-01-23 21:54:02 -0300

    feat: Create and adjust the UploadImage component
    closes #24

--- src/components/UploadImage/index.js
+++ src/components/UploadImage/index.js
@@ -1,22 +1,22 @@
 import { Component } from 'pet-dex-utilities';
 import './index.scss';
 import placeholderImage from './img/placeholder.svg';
-import plus from './img/plus.svg';
-import photoAdded from './img/photo-added.svg';
+import plusIcon from './img/plus-icon.svg';
+import photoIcon from './img/photo-icon.svg';
 
 const html = `
-  <div class="upload-image-container" data-select="upload-container">
-    <div class="upload-image-container__animation" data-select="upload-animation">
+  <div class="upload-container">
+    <div class="upload-container__animation" data-select="upload-animation">
       <div class="circle"></div>
       <div class="circle-duplicate"></div>
     </div>
-    <label for="input-file" class="upload-image-container__label" data-select="upload-label">
-      <div class="image-container" data-select="upload-image-container">
-        <img class="profile-image" data-select="upload-icon" src="${placeholderImage}" alt="Imagem de upload">
-        <img class="preview-image hidden" data-select="upload-preview">
+    <label for="input-file" class="upload-container__label">
+      <div class="image-container">
+        <img class="placeholder-image" src="${placeholderImage}" alt="Placeholder">
+        <img class="preview-image hidden" data-select="upload-preview" alt="Imagem carregada">
       </div>
-      <div class='button' data-select="upload-button">
-        <img data-select="upload-button-img" src="${plus}">
+      <div class='button'>
+        <img data-select="button-icon" src="${plusIcon}">
       </div>
       <input id="input-file" name="input-file" type="file" accept="image/*" data-select="upload-input">
     </label>
@@ -27,7 +27,7 @@ export default function UploadImage() {
   Component.call(this, { html });
 
   const previewImage = this.selected.get('upload-preview');
-  const buttonImg = this.selected.get('upload-button-img');
+  const buttonIcon = this.selected.get('button-icon');
 
   this.selected.get('upload-input').addEventListener('change', (e) => {
     const inputTarget = e.target;
@@ -39,7 +39,7 @@ export default function UploadImage() {
         const readerTarget = l.target;
         previewImage.src = readerTarget.result;
         previewImage.classList.remove('hidden');
-        buttonImg.src = photoAdded;
+        buttonIcon.src = photoIcon;
       });
 
       reader.readAsDataURL(file);
