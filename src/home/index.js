519574def03dd49bda36fd734b6a90af3b637574
JonasGz <jonaschagasweb@gmail.com>
2024-02-09 19:46:30 -0300

    create sidemenu

--- src/home/index.js
+++ src/home/index.js
@@ -18,4 +18,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const $content = selected.get('content');
   const noPetRegirestedPage = new NoPetRegirestedPage();
   noPetRegirestedPage.mount($content);
+  
+  const $hamburgerMenu = navigation.selected.get('hamburgerMenu');
+  const $exitMenu = sideMenu.selected.get('exitMenu');
+  const $itemsMenu = Array.from(sideMenu.selected.get('menuitens').childNodes);
+  const $itemsMenuHTML = $itemsMenu.filter((node) => node.nodeType === Node.ELEMENT_NODE && node.tagName.toLowerCase() === 'li');
+  const breakpointDesktop = 1024;
+
+  function scrollTo(element) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+
+  function clickScroll(element, targetElement) {
+    if (window.innerWidth < breakpointDesktop) {
+      element.addEventListener('click', () => {
+        scrollTo(targetElement);
+      });
+    }
+  }
+
+  function activeClassMenu(item, itemsMenu) {
+    itemsMenu.forEach((element) => {
+      element.classList.remove('side-menu-content__menuitens--active');
+    });
+
+    item.classList.add('side-menu-content__menuitens--active');
+
+    if (window.innerWidth < breakpointDesktop) {
+      scrollTo($navigation);
+    }
+  }
+
+  clickScroll($hamburgerMenu, $sidemenu);
+  clickScroll($exitMenu, $navigation);
+
+  $itemsMenuHTML.forEach((item) => {
+    item.addEventListener('click', () => {
+      activeClassMenu(item, $itemsMenuHTML);
+    });
+  });
+
 });
