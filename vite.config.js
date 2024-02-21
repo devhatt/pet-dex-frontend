97346579e325f03f9a964cc87c05d9fdb7c19c63
Jessica Rodrigues <100847406+jeressy@users.noreply.github.com>
2024-02-20 23:59:23 -0300

    Adiciona favicons à Home e configura o VitePWA no arquivo `vite.config.js` com o manifesto (#72)
    * test
    
    * Issue 37 - Adiciona favicons a home e configura o VitePWA no vit.config.js
    
    Adiciona os favicons necessários e configura o manifesto no plugin VitePWA no arquivo `vite.config.js`
    
    ---------
    
    Co-authored-by: Alexandre Gomes <alecell.dev@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
@@ -27,4 +28,47 @@ export default defineConfig({
       '~styles': resolve(__dirname, 'src/styles'),
     },
   },
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'PetDex',
+        short_name: 'PetDex',
+        icons: [
+          {
+            src: "/favicon/android-icon-36x36.ico",
+            sizes: "36x36",
+            type: "image/png"
+          },
+          {
+            src: "/favicon/android-icon-48x48.ico",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "/favicon/android-icon-72x72.ico",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "/favicon/android-icon-96x96.ico",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "/favicon/android-icon-144x144.ico",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "/favicon/android-icon-192x192.ico",
+            sizes: "192x192",
+            type: "image/png"
+          }
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone'
+      }
+    })
+  ]
 });
