e280163d233c5e9ab1eeb63330502b070eefcb8d
Bruno <buunin.bee@gmail.com>
2024-03-07 16:22:25 -0300

    adicionando imgs de qaundo o item tiver ativo

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -9,17 +9,17 @@ const events = ['click', 'touchstart'];
 const html = `
 <div class="slider-wrapper" data-select="slider-wrapper">
   <ul class="slider-wrapper__slide" data-select="slider-wrapper__slide"> 
-     <li role="widget" aria-checked="false" class="slider-wrapper__item" data-select="slider-small">
-      <img class="slider-wrapper__image" src="${sizeSmall}" alt="icon do tamanho pequeno do pet">
+     <li role="checkbox" aria-checked="false" class="slider-wrapper__item" data-select="slider-small">     
+      <img class="slider-wrapper__image" data-select="slider-wrapper__image" src="${sizeSmall}" alt="icon do tamanho pequeno do pet">
       <h3>Small</h3>
       <p>under 14kg</p>
      </li>
-     <li role="widget" aria-checked="false" class="slider-wrapper__item active" data-select="slider-medium">
+     <li role="checkbox" aria-checked="false" class="slider-wrapper__item active" data-select="slider-medium">
       <img class="slider-wrapper__image" src="${sizeMedium}" alt="icon do tamanho medio do pet">
       <h3>Medium</h3>
       <p>14-25kg</p>
      </li>
-     <li role="widget" aria-checked="false" class="slider-wrapper__item" data-select="slider-large">
+     <li role="checkbox" aria-checked="false" class="slider-wrapper__item" data-select="slider-large">
       <img class="slider-wrapper__image" da src="${sizeLarge}" alt="icon do tamanho grande do pet">
       <h3>Large</h3>
       <p>over 25kg</p>
@@ -44,6 +44,7 @@ Slider.prototype = Object.assign(Slider.prototype, Component.prototype, {
     childrens.map((element) => {
       element.addEventListener('click', (event) => {
         if (element.classList.contains('active')) {
+          this.ChangeImgActive(element);
           element.classList.add('active');
           event.target.setAttribute('aria-checked', 'true');
         } else {
@@ -54,8 +55,39 @@ Slider.prototype = Object.assign(Slider.prototype, Component.prototype, {
           });
           element.classList.add('active');
         }
+        this.ChangeImgDefault(element);
       });
       return this;
     });
+  },
+
+  ChangeImgActive(element) {
+    const item = element.children[0].attributes.src.value;
+
+    if (item === '/components/slider/img/size-small.svg') {
+      element.children[0].attributes.src.value =
+        '/components/slider/img/size-small-active.svg';
+    } else if (item === '/components/slider/img/size-medium.svg') {
+      element.children[0].attributes.src.value =
+        '/components/slider/img/size-medium-active.svg';
+    } else if (item === '/components/slider/img/size-large.svg') {
+      element.children[0].attributes.src.value =
+        '/components/slider/img/size-large-active.svg';
+    }
+  },
+
+  ChangeImgDefault(element) {
+    const item = element.children[0].attributes.src.value;
+
+    if (item === '/components/slider/img/size-small-active.svg') {
+      element.children[0].attributes.src.value =
+        '/components/slider/img/size-small.svg';
+    } else if (item === '/components/slider/img/size-medium-active.svg') {
+      element.children[0].attributes.src.value =
+        '/components/slider/img/size-medium.svg';
+    } else if (item === '/components/slider/img/size-large-active.svg') {
+      element.children[0].attributes.src.value =
+        '/components/slider/img/size-large.svg';
+    }
   },
 });
