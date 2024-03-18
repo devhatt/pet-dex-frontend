04ede37c47f00216e68d74823b6049493876c864
Nathalia <64925426+nathalia-84@users.noreply.github.com>
2024-03-18 00:23:02 -0300

    feat: create toggle component (#77)
    * feat: Implement toggle component adhering to Material Design with animated switch and external event listeners
    
    fix #71
    
    * feat: Implement toggle component adhering to Material Design with animated switch and external event listeners
    
    fix #71
    
    * Made some adjustments according to reviews
    
    * Write modifiers according to BEM
    
    * Adjust Toggle function
    
    * Solved conflicts
    
    * Make adjustments according to reviews
    
    * Made adjustments according to reviews, changed scss file structure
    
    * Create setToggle function
    
    * Adjust scss and setToggle method structure

---
+++ src/components/Toggle/index.js
@@ -0,0 +1,35 @@
+import './index.scss';
+import { Component, createIDFactory } from 'pet-dex-utilities';
+
+const generateID = createIDFactory('toggle');
+
+const events = ['toggle'];
+
+const html = `
+  <div class="toggle-container">
+    <input type="checkbox" class="toggle-container__input" data-select="toggle-input">
+    <label class="toggle-container__label" data-select="toggle-label"></label>
+  </div>
+`;
+
+export default function Toggle({ checked = false } = {}) {
+  Component.call(this, { html, events });
+
+  const id = generateID();
+  this.selected.get('toggle-input').setAttribute('id', id);
+  this.selected.get('toggle-label').setAttribute('for', id);
+
+  this.setToggle(checked, false);
+
+  this.selected.get('toggle-input').addEventListener('change', () => this.emitToggle());
+}
+
+Toggle.prototype = Object.assign(Toggle.prototype, Component.prototype, {
+  emitToggle() {
+    this.emit('toggle', this.selected.get('toggle-input').checked);
+  },
+  setToggle(checked, emit = true) {
+    this.selected.get('toggle-input').checked = checked;
+    if (emit) this.emitToggle();
+  },
+});
