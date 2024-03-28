410085f046bba2916310f056b602d9baaf06e4f2
Douglas Murita <douglasb008@gmail.com>
2024-03-28 18:15:27 -0300

    docs: Comment changes for my component work in hard coded

--- src/home/index.js
+++ src/home/index.js
@@ -1,17 +1,18 @@
 import { extractElements } from 'pet-dex-utilities';
 import Navigation from './components/Navigation';
-import PetRegisterPage from './components/PetRegisterPage';
+import NoPetRegirestedPage from './components/NoPetRegirestedPage';
+// import PetRegisterPage from './components/PetRegisterPage';
 import SideMenu from './components/SideMenu';
 import initializeScrollable from './utils/scrollable-sidemenu';
 
-import afghanHound from './components/PetRegisterPage/images/afghanHound.svg';
-import akita from './components/PetRegisterPage/images/akita.svg';
-import beagle from './components/PetRegisterPage/images/beagle.svg';
-import bichonFrise from './components/PetRegisterPage/images/bichonFrise.svg';
-import borderCollie from './components/PetRegisterPage/images/borderCollie.svg';
-import boxer from './components/PetRegisterPage/images/boxer.svg';
-import chowChow from './components/PetRegisterPage/images/chowChow.svg';
-import mixedBreed from './components/PetRegisterPage/images/mixedBreed.svg';
+// import afghanHound from './components/PetRegisterPage/images/afghanHound.svg';
+// import akita from './components/PetRegisterPage/images/akita.svg';
+// import beagle from './components/PetRegisterPage/images/beagle.svg';
+// import bichonFrise from './components/PetRegisterPage/images/bichonFrise.svg';
+// import borderCollie from './components/PetRegisterPage/images/borderCollie.svg';
+// import boxer from './components/PetRegisterPage/images/boxer.svg';
+// import chowChow from './components/PetRegisterPage/images/chowChow.svg';
+// import mixedBreed from './components/PetRegisterPage/images/mixedBreed.svg';
 
 import './index.scss';
 
@@ -28,63 +29,64 @@ document.addEventListener('DOMContentLoaded', () => {
   const navigation = new Navigation();
   navigation.mount($navigation);
 
-  // const $content = selected.get('content');
-  // const noPetRegirestedPage = new NoPetRegirestedPage();
-  // noPetRegirestedPage.mount($content);
-
-  const cards = [
-    {
-      title: 'Akita',
-      imgSrc: akita,
-      imgAlt: 'akita',
-    },
-    {
-      title: 'Boxer',
-      imgSrc: boxer,
-      imgAlt: 'boxer',
-    },
-    {
-      title: 'Beagle',
-      imgSrc: beagle,
-      imgAlt: 'beagle',
-    },
-    {
-      title: 'Afghan Hound',
-      imgSrc: afghanHound,
-      imgAlt: 'afghan hound',
-    },
-    {
-      title: 'Bichon Frise',
-      imgSrc: bichonFrise,
-      imgAlt: 'bichon frise',
-    },
-    {
-      title: 'Chow Chow',
-      imgSrc: chowChow,
-      imgAlt: 'chow chow',
-    },
-    {
-      title: 'Border Collie',
-      imgSrc: borderCollie,
-      imgAlt: 'border collie',
-    },
-    {
-      title: 'Mixed Breed',
-      imgSrc: mixedBreed,
-      imgAlt: 'mixed breed',
-    },
-  ];
-
   const $content = selected.get('content');
-  const petRegisterPage = new PetRegisterPage({ cards });
-  petRegisterPage.mount($content);
-  petRegisterPage.listen('select:card', (card) => {
-    console.log('card da home: ', card.elements);
-  });
+  const noPetRegirestedPage = new NoPetRegirestedPage();
+  noPetRegirestedPage.mount($content);
 
-  petRegisterPage.listen('submit-pet', (card) => {
-    console.log('submit da home: ', card);
-  });
+  // Hard coded do meu componente
+  // const cards = [
+  //   {
+  //     title: 'Akita',
+  //     imgSrc: akita,
+  //     imgAlt: 'akita',
+  //   },
+  //   {
+  //     title: 'Boxer',
+  //     imgSrc: boxer,
+  //     imgAlt: 'boxer',
+  //   },
+  //   {
+  //     title: 'Beagle',
+  //     imgSrc: beagle,
+  //     imgAlt: 'beagle',
+  //   },
+  //   {
+  //     title: 'Afghan Hound',
+  //     imgSrc: afghanHound,
+  //     imgAlt: 'afghan hound',
+  //   },
+  //   {
+  //     title: 'Bichon Frise',
+  //     imgSrc: bichonFrise,
+  //     imgAlt: 'bichon frise',
+  //   },
+  //   {
+  //     title: 'Chow Chow',
+  //     imgSrc: chowChow,
+  //     imgAlt: 'chow chow',
+  //   },
+  //   {
+  //     title: 'Border Collie',
+  //     imgSrc: borderCollie,
+  //     imgAlt: 'border collie',
+  //   },
+  //   {
+  //     title: 'Mixed Breed',
+  //     imgSrc: mixedBreed,
+  //     imgAlt: 'mixed breed',
+  //   },
+  // ];
+
+  // const $content = selected.get('content');
+  // const petRegisterPage = new PetRegisterPage({ cards });
+  // petRegisterPage.mount($content);
+  // petRegisterPage.listen('select:card', (card) => {
+  //   console.log('card da home: ', card);
+  // });
+
+  // petRegisterPage.listen('submit-pet', (card) => {
+  //   console.log('submit da home: ', card);
+  // });
 
   const $hamburgerMenu = navigation.selected.get('hamburger-menu');
   const $exitMenu = sideMenu.selected.get('exitMenu');
