4d22cdf0ff5f1b281bb28f7e6b9b7e75d9b92fda
juliaM <35346206+juliaam@users.noreply.github.com>
2024-04-19 09:34:50 -0300

    fix: mount callback (#186)

--- src/utils/breakpoints/breakpoints.js
+++ src/utils/breakpoints/breakpoints.js
@@ -1,6 +1,7 @@
 import variables from '../../styles/breakpoints.module.scss';
 
-const { extraSmallSize, smallSize, mediumSize, largeSize, largestSize } = variables;
+const { extraSmallSize, smallSize, mediumSize, largeSize, largestSize } =
+  variables;
 
 const events = new Map();
 events.set('from320', new Set());
@@ -9,6 +10,14 @@ events.set('from667', new Set());
 events.set('from1024', new Set());
 events.set('from1280', new Set());
 
+const obj = {
+  from320: window.matchMedia(`(min-width: ${extraSmallSize})`),
+  from360: window.matchMedia(`(min-width: ${smallSize})`),
+  from667: window.matchMedia(`(min-width: ${mediumSize})`),
+  from1024: window.matchMedia(`(min-width: ${largeSize})`),
+  from1280: window.matchMedia(`(min-width: ${largestSize})`),
+};
+
 export function listenBreakpoint(breakpoint, callback) {
   const callbacks = events.get(breakpoint);
   if (!callbacks) {
@@ -16,6 +25,7 @@ export function listenBreakpoint(breakpoint, callback) {
     return;
   }
   callbacks.add(callback);
+  callback(obj[breakpoint].matches);
 }
 
 export function unlistenBreakpoint(breakpoint, callback) {
@@ -26,13 +36,6 @@ export function unlistenBreakpoint(breakpoint, callback) {
   }
   callbacks.delete(callback);
 }
-const obj = {
-  from320: window.matchMedia(`(min-width: ${extraSmallSize})`),
-  from360: window.matchMedia(`(min-width: ${smallSize})`),
-  from667: window.matchMedia(`(min-width: ${mediumSize})`),
-  from1024: window.matchMedia(`(min-width: ${largeSize})`),
-  from1280: window.matchMedia(`(min-width: ${largestSize})`),
-};
 
 Object.keys(obj).forEach((key) => {
   obj[key].addEventListener('change', (e) => {
