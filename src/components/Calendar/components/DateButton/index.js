04b094bef5669c8d8fe5366613ab2f0f1d137a4f
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-04-26 00:20:53 -0300

    chore: update branch

---
+++ src/components/Calendar/components/DateButton/index.js
@@ -0,0 +1,28 @@
+import { Component } from 'pet-dex-utilities';
+import './index.scss';
+
+const html = `
+  <li data-select="date" class="date"><button></button></li>
+`;
+
+export default function DateButton(date) {
+  Component.call(this, { html });
+
+  const $button = this.selected.get('date').children[0];
+  $button.innerText = date;
+}
+
+DateButton.prototype = Object.assign(
+  DateButton.prototype,
+  Component.prototype,
+  {
+    active() {
+      this.selected.get('date').classList.add('active');
+    },
+
+    setYear(year) {
+      const $button = this.selected.get('date').children[0];
+      $button.innerText = year;
+    },
+  },
+);
