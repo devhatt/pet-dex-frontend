f82dc1856efee237240c04ff7cd503ad1dab8ca9
Marce <assumpcaom23@gmail.com>
2024-04-25 21:04:42 -0300

    refactor: refatora código da página de peso para ficar mais legível e modular

--- src/home/components/PetWeightPage/index.js
+++ src/home/components/PetWeightPage/index.js
@@ -24,103 +24,137 @@ const html = `
   </div>;
 `;
 
-export default function PetWeightPage() {
+export default function PetWeightPage({ petPhoto }) {
   Component.call(this, { html });
-
-  const $container = this.selected.get('container');
-  const $imageContainer = this.selected.get('image-container');
-  const $sliderContainer = this.selected.get('slider-container');
-  const $inputsContainer = this.selected.get('input-container');
-
-  this.image = new UploadImage();
-  this.slider = new RangeSlider();
-  this.input = new TextInput({
-    placeholder: 'Peso',
-    assetUrl: '',
-    assetPosition: 'prefix',
-    variation: 'standard',
-  });
-  this.radioKG = new RadioButton({
-    check: true,
-    text: 'KG',
-    value: 'kg',
-    name: 'weight-unit',
-  });
-  this.radioLB = new RadioButton({
-    text: 'LB',
-    value: 'lb',
-    name: 'weight-unit',
-  });
-  this.button = new Button({
-    text: 'Continuar',
-    isFullWidth: false,
-    isDisabled: false,
-  });
-
-  this.image.selected
-    .get('image-preview')
-    .classList.add('pet-weight-page__image');
-
-  this.slider.selected
-    .get('range-slider')
-    .classList.add('pet-weight-page__slider');
-  this.slider.selected
-    .get('range-slider-value')
-    .classList.add('pet-weight-page__value');
-  this.input.selected.get('input-text').classList.add('pet-weight-page__input');
-  this.input.selected
-    .get('input-text-container')
-    .classList.add('pet-weight-page__input-container');
-  this.radioKG.selected
-    .get('radio-container')
-    .classList.add('pet-weight-page__radio');
-  this.radioLB.selected
-    .get('radio-container')
-    .classList.add('pet-weight-page__radio');
-  this.button.selected.get('button').classList.add('pet-weight-page__button');
-
-  this.slider.listen('value:change', (value) => {
-    this.weight = Number(value.toFixed(1));
-    this.input.setValue(this.weight);
-  });
-
-  this.input.listen('value:changed', (value) => {
-    if (this.weight !== value) {
-      const numericValue = parseFloat(value);
-      if (!numericValue.isNaN && this.weight !== numericValue) {
-        this.weight = Number(value);
-        this.slider.setValue(this.weight);
-      }
-    }
-  });
-
-  const weightUnit = () => {
-    if (this.radioKG.isChecked()) {
-      return this.radioKG.getValue();
-    }
-    return this.radioLB.getValue();
-  };
-
-  this.button.listen('click', () => {
-    const finalWeightUnit = weightUnit();
-    const finalWeight = this.weight;
-    const event = new CustomEvent('weight:submit', {
-      detail: { finalWeight, finalWeightUnit },
-
-      bubbles: true,
-    });
-    $container.dispatchEvent(event);
-  });
-
-  this.image.mount($imageContainer);
-  this.slider.mount($sliderContainer);
-  this.input.mount($inputsContainer);
-  this.radioKG.mount($inputsContainer);
-  this.radioLB.mount($inputsContainer);
-  this.button.mount($container);
+  this.initializeComponents();
+  this.setupEventListeners();
+  this.styleComponents();
+  this.petPhoto = petPhoto;
 }
 
 PetWeightPage.prototype = Object.assign(
   PetWeightPage.prototype,
   Component.prototype,
+  {
+    initializeComponents() {
+      const $container = this.selected.get('container');
+      const $imageContainer = this.selected.get('image-container');
+      const $sliderContainer = this.selected.get('slider-container');
+      const $inputsContainer = this.selected.get('input-container');
+
+      this.setupComponents(
+        $container,
+        $imageContainer,
+        $sliderContainer,
+        $inputsContainer,
+      );
+    },
+
+    setupComponents(
+      $container,
+      $imageContainer,
+      $sliderContainer,
+      $inputsContainer,
+    ) {
+      this.image = new UploadImage();
+      this.slider = new RangeSlider();
+      this.input = new TextInput({
+        placeholder: 'Peso',
+        assetUrl: '',
+        assetPosition: 'prefix',
+        variation: 'standard',
+      });
+      this.radioKG = new RadioButton({
+        check: true,
+        text: 'KG',
+        value: 'kg',
+        name: 'weight-unit',
+      });
+      this.radioLB = new RadioButton({
+        text: 'LB',
+        value: 'lb',
+        name: 'weight-unit',
+      });
+      this.button = new Button({
+        text: 'Continuar',
+        isFullWidth: false,
+        isDisabled: false,
+      });
+
+      // Mount components
+      this.image.mount($imageContainer);
+      this.slider.mount($sliderContainer);
+      this.input.mount($inputsContainer);
+      this.radioKG.mount($inputsContainer);
+      this.radioLB.mount($inputsContainer);
+      this.button.mount($container);
+    },
+
+    styleComponents() {
+      // Add classes to components
+      this.addClassToComponent(
+        this.image.selected.get('image-preview'),
+        'pet-weight-page__image',
+      );
+      this.addClassToComponent(
+        this.slider.selected.get('range-slider'),
+        'pet-weight-page__slider',
+      );
+      this.addClassToComponent(
+        this.slider.selected.get('range-slider-value'),
+        'pet-weight-page__value',
+      );
+      this.addClassToComponent(
+        this.input.selected.get('input-text'),
+        'pet-weight-page__input',
+      );
+      this.addClassToComponent(
+        this.input.selected.get('input-text-container'),
+        'pet-weight-page__input-container',
+      );
+      this.addClassToComponent(
+        this.radioKG.selected.get('radio-container'),
+        'pet-weight-page__radio',
+      );
+      this.addClassToComponent(
+        this.radioLB.selected.get('radio-container'),
+        'pet-weight-page__radio',
+      );
+      this.addClassToComponent(
+        this.button.selected.get('button'),
+        'pet-weight-page__button',
+      );
+    },
+
+    addClassToComponent(element, className) {
+      element.classList.add(className);
+    },
+
+    setupEventListeners() {
+      this.slider.listen('value:change', (value) => {
+        this.weight = Number(value.toFixed(1));
+        this.input.setValue(this.weight);
+      });
+
+      this.input.listen('value:change', (value) => {
+        const numericValue = parseFloat(value);
+        if (!numericValue.isNaN && this.weight !== numericValue) {
+          this.weight = numericValue;
+          this.slider.setValue(this.weight);
+        }
+      });
+
+      function weightUnit() {
+        return this.radioKG.isChecked()
+          ? this.radioKG.getValue()
+          : this.radioLB.getValue();
+      }
+
+      this.button.listen('click', () => {
+        const finalWeightUnit = weightUnit();
+        const finalWeight = this.weight;
+        this.emit('weight', finalWeight, finalWeightUnit);
+      });
+    },
+  },
 );
