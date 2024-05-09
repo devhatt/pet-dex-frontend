c9b43a1901a7c7a9480020591ab5a539547b68b1
DominMFD <mfdmatheus@hotmail.com>
2024-05-09 16:01:07 -0300

    refactor: refactor sliding component

--- src/components/Sliding/index.js
+++ src/components/Sliding/index.js
@@ -1,31 +1,125 @@
 import { Component } from 'pet-dex-utilities';
-import './index.scss';
 import { initializeSwiper, makeSwipable } from '../../utils/swiper';
+import './index.scss';
 
-const events = [];
+const events = [
+  'slide:add',
+  'slide:next',
+  'slide:prev',
+  'slide:remove',
+  'slides:clear',
+];
 
 const html = `
 <div class="sliding">
   <div class="sliding__content" data-select="sliding-content">
-    <div class="sliding__content__slide">1</div>
-    <div class="sliding__content__slide">2</div>
-    <div class="sliding__content__slide">3</div>
-    <div class="sliding__content__slide">4</div>
-    <div class="sliding__content__slide">5</div>
   </div>
 </div>`;
 
-export default function Sliding({ items = [] }) {
+export default function Sliding({ slides = [] }) {
   Component.call(this, { html, events });
 
   this.slideIndex = 0;
-  this.slides = this.selected.get('sliding-content').children;
+  this.slides = [];
 
-  this.setSlide(this.slideIndex);
+  this.updateSlides = () => {
+    this.slides = Array.from(this.selected.get('sliding-content').children);
+  };
+
+  this.updateWidth = () => {
+    this.selected.get('sliding-content').style.width =
+      `${this.slides.length * 100}%`;
+  };
+
+  slides.forEach((item) => this.add(item));
+
+  makeSwipable(this.selected.get('sliding-content'));
+  initializeSwiper();
+
+  this.selected
+    .get('sliding-content')
+    .addEventListener('swipe-left', () => this.next());
+  this.selected
+    .get('sliding-content')
+    .addEventListener('swipe-right', () => this.prev());
+
+  this.listen('mount', () =>
+    document.addEventListener('click', this.setSlide(this.slides[0])),
+  );
+  this.listen('unmount', () =>
+    document.addEventListener('click', this.clearItems()),
+  );
+
+  this.setSlide = (slide) => {
+    this.slides.forEach((item) => {
+      if (item === slide) {
+        item.classList.add('sliding__content__slide--active');
+      } else {
+        item.classList.remove('sliding__content__slide--active');
+      }
+    });
+  };
 }
 
 Sliding.prototype = Object.assign(Sliding.prototype, Component.prototype, {
-  setSlide() {},
-  next() {},
-  prev() {},
+  add(slide) {
+    slide.classList.add('sliding__content__slide');
+    this.selected.get('sliding-content').appendChild(slide);
+
+    this.updateSlides();
+    this.updateWidth();
+
+    this.emit('slide:add', slide);
+  },
+
+  remove(slide) {
+    this.selected.get('sliding-content').removeChild(slide);
+
+    this.updateSlides();
+    this.updateWidth();
+
+    this.emit('slide:remove', slide);
+  },
+
+  next() {
+    this.slideIndex += 1;
+
+    if (this.slideIndex > this.slides.length - 1) this.slideIndex = 0;
+
+    const slide =
+      this.selected.get('sliding-content').children[this.slideIndex];
+    const slideWidth =
+      this.selected.get('sliding-content').clientWidth / this.slides.length;
+
+    this.selected.get('sliding-content').style.transform =
+      `translateX(${-this.slideIndex * slideWidth}px)`;
+
+    this.setSlide(slide);
+
+    this.emit('slide:next', slide);
+  },
+
+  prev() {
+    this.slideIndex -= 1;
+
+    if (this.slideIndex < 0) this.slideIndex = this.slides.length - 1;
+
+    const slide =
+      this.selected.get('sliding-content').children[this.slideIndex];
+    const slideWidth =
+      this.selected.get('sliding-content').clientWidth / this.slides.length;
+
+    this.selected.get('sliding-content').style.transform =
+      `translateX(${-this.slideIndex * slideWidth}px)`;
+
+    this.setSlide(slide);
+
+    this.emit('slide:prev', slide);
+  },
+
+  clear() {
+    this.slides.forEach((slide) => this.remove(slide));
+
+    this.emit('slides:clear');
+  },
 });
--- src/stories/Sliding.stories.js
+++ src/stories/Sliding.stories.js
@@ -1,4 +1,29 @@
 import Sliding from '../components/Sliding';
+import Button from '../components/Button';
+
+const button = new Button({
+  text: 'Bom dia',
+  isFullWidth: true,
+});
+
+const $buttonContainer = document.createElement('div');
+button.mount($buttonContainer);
+
+const button2 = new Button({
+  text: 'Tamo ai',
+  isFullWidth: true,
+});
+
+const $button2Container = document.createElement('div');
+button2.mount($button2Container);
+
+const button3 = new Button({
+  text: 'oi',
+  isFullWidth: true,
+});
+
+const $button3Container = document.createElement('div');
+button3.mount($button3Container);
 
 export default {
   title: 'Components/Sliding',
@@ -10,9 +35,13 @@ export default {
 
     return $container;
   },
-  argsTypes: {},
+  argsTypes: {
+    slides: { control: 'object', defaultValue: [] },
+  },
 };
 
 export const Default = {
-  args: {},
+  args: {
+    slides: [$buttonContainer, $button2Container, $button3Container],
+  },
 };
