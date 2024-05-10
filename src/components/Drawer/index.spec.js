0216f33bc17e9d53a441144e1c6a59f28a53f92f
Rafael Lima <srafalimacosta@gmail.com>
2024-05-10 12:31:03 -0300

    test: add new ones

--- src/components/Drawer/index.spec.js
+++ src/components/Drawer/index.spec.js
@@ -1,33 +1,57 @@
 import { fireEvent } from '@testing-library/dom';
+import { render } from '@testing-library/vanilla';
 import { describe, expect, it } from 'vitest';
+import Drawer from '.';
 import Button from '../Button';
-import Drawer from './index';
 
-const drawer = new Drawer({
+const makeSut = () => render(new Drawer({
   title: 'Add dates',
   content: new Button({
     text: 'Cadastrar pet',
     isFullWidth: true,
     isDisabled: false,
   }),
-});
+}))
+
 
 describe('Drawer', () => {
   describe('when mount', () => {
     it('renders the title', () => {
+      const drawer = makeSut()
       drawer.open();
 
       expect(drawer.selected.get('title').textContent).toBe('Add dates');
     });
+
+    it('mount the content', () => {
+      const drawer = makeSut();
+
+      const mountSpy = vi.spyOn(drawer, 'mount')
+
+      drawer.open()
+
+      expect(mountSpy).toHaveBeenCalled();
+    })
   });
 
   describe('when unmount', () => {
     it('remove event listeners', () => {
-      // todo
+      const drawer = makeSut()
+      const removeEventListenerSpy = vi.spyOn(window, 'removeEventListener');
+      const onEscapeKeySpy = vi.spyOn(drawer, 'onEscapeKey');
+
+      drawer.open();
+      drawer.emit('unmount');
+
+      expect(removeEventListenerSpy).toHaveBeenCalledWith('keydown', onEscapeKeySpy);
+
+      removeEventListenerSpy.mockRestore();
+      onEscapeKeySpy.mockRestore();
     });
   });
 
   it('closes when Esc is pressed', () => {
+    const drawer = makeSut()
     drawer.open();
 
     fireEvent.keyDown(document, { key: 'Escape' });
@@ -38,6 +62,7 @@ describe('Drawer', () => {
   });
 
   it('closes when the close button is clicked', () => {
+    const drawer = makeSut()
     drawer.open();
 
     fireEvent.click(drawer.selected.get('close'));
@@ -46,6 +71,4 @@ describe('Drawer', () => {
       'drawer--open',
     );
   });
-
-  // scroll com texto grande
 });
