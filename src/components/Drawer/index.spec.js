db02042215002f241443a84cea14f624120e1740
Alecell <alecell.dev@gmail.com>
2024-05-20 21:52:11 -0300

    style: prettier fixes

--- src/components/Drawer/index.spec.js
+++ src/components/Drawer/index.spec.js
@@ -1,23 +1,27 @@
+/* eslint-disable testing-library/prefer-user-event */
+/* eslint-disable no-restricted-syntax */
 import { fireEvent } from '@testing-library/dom';
 import { render } from '@testing-library/vanilla';
 import { describe, expect, it } from 'vitest';
 import Drawer from '.';
 import Button from '../Button';
 
-const makeSut = () => render(new Drawer({
-  title: 'Add dates',
-  content: new Button({
-    text: 'Cadastrar pet',
-    isFullWidth: true,
-    isDisabled: false,
-  }),
-}))
-
+const makeSut = () =>
+  render(
+    new Drawer({
+      title: 'Add dates',
+      content: new Button({
+        text: 'Cadastrar pet',
+        isFullWidth: true,
+        isDisabled: false,
+      }),
+    }),
+  );
 
 describe('Drawer', () => {
   describe('on mount', () => {
     it('renders', () => {
-      const drawer = makeSut()
+      const drawer = makeSut();
 
       drawer.open();
 
@@ -28,19 +32,22 @@ describe('Drawer', () => {
 
   describe('on unmount', () => {
     it('remove event listeners', () => {
-      const drawer = makeSut()
+      const drawer = makeSut();
       const removeEventListenerSpy = vi.spyOn(window, 'removeEventListener');
       const onEscapeKeySpy = vi.spyOn(drawer, 'onEscapeKey');
 
       drawer.open();
       drawer.emit('unmount');
 
-      expect(removeEventListenerSpy).toHaveBeenCalledWith('keydown', onEscapeKeySpy);
+      expect(removeEventListenerSpy).toHaveBeenCalledWith(
+        'keydown',
+        onEscapeKeySpy,
+      );
     });
   });
 
   it('closes when Esc is pressed', () => {
-    const drawer = makeSut()
+    const drawer = makeSut();
 
     drawer.open();
 
@@ -52,7 +59,7 @@ describe('Drawer', () => {
   });
 
   it('closes when the close button is clicked', () => {
-    const drawer = makeSut()
+    const drawer = makeSut();
     const closeSpy = vi.spyOn(drawer, 'close');
 
     drawer.open();
