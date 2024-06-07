99e5bad9a814cd031051e0ffaade88d24191591a
Matheus <mf635097@gmail.com>
2024-06-07 14:08:21 -0300

    refactor: improve error message display for form inputs

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -18,12 +18,14 @@ const html = `
     <div class="login-form-container">
       <h2 class="login-form-container__title">Sua petconta</h2>
       <form data-select="login-form" class="login-form-container__login-form" action="submit">
-        <div data-select="input-container" class="input-container"></div>
-        <span data-select="error-message" class="error-message">Error Message</span>
+        <div data-select="email-input-container"></div>
+        <span data-select="email-error-message" class="error-message"></span>
+        <div data-select="password-input-container"></div>
+        <span data-select="password-error-message" class="error-message"></span>
       
         <div class="login-form-container__login-options">
           <div data-select="remember-option" class="remember-option">
-            <p>Remember me</P>
+            <p>Remember me</p>
           </div>
           <a href="#" class="forgot-password">Esqueceu a senha?</a>
         </div>
@@ -50,9 +52,11 @@ const html = `
 export default function LoginForm() {
   Component.call(this, { html });
   const $loginForm = this.selected.get('login-form');
-  const $inputContainer = this.selected.get('input-container');
+  const $emailInputContainer = this.selected.get('email-input-container');
+  const $passwordInputContainer = this.selected.get('password-input-container');
   const $rememberOption = this.selected.get('remember-option');
-  const $errorMessage = this.selected.get('error-message');
+  const $emailErrorMessage = this.selected.get('email-error-message');
+  const $passwordErrorMessage = this.selected.get('password-error-message');
 
   const emailInput = new TextInput({ placeholder: 'E-mail' });
   const passwordInput = new TextInput({
@@ -67,8 +71,8 @@ export default function LoginForm() {
     isDisabled: true,
   });
 
-  emailInput.mount($inputContainer);
-  passwordInput.mount($inputContainer);
+  emailInput.mount($emailInputContainer);
+  passwordInput.mount($passwordInputContainer);
   submitButton.mount($loginForm);
   toggle.mount($rememberOption);
 
@@ -96,24 +100,30 @@ export default function LoginForm() {
   submitButton.listen('click', () => {
     const email = emailInput.selected.get('input-text').value;
     const password = passwordInput.selected.get('input-text').value;
+    let validFields = true;
 
     if (!this.validateEmail(email)) {
-      $errorMessage.classList.add('show-error');
-      $errorMessage.innerText = 'E-mail inválido';
+      validFields = false;
+      $emailErrorMessage.classList.add('show-error');
+      $emailErrorMessage.innerText = 'E-mail inválido';
       emailInput.inputError();
-      return;
+    } else {
+      $emailErrorMessage.classList.remove('show-error');
     }
 
     if (!this.validatePassword(password)) {
-      $errorMessage.classList.add('show-error');
-      $errorMessage.innerText =
+      validFields = false;
+      $passwordErrorMessage.classList.add('show-error');
+      $passwordErrorMessage.innerText =
         'Senha inválida. Sua senha deve conter no mínimo 10 caracteres, incluindo pelo menos um caractere especial e uma letra maiúscula.';
       passwordInput.inputError();
-      return;
+    } else {
+      $passwordErrorMessage.classList.remove('show-error');
     }
 
-    $errorMessage.classList.remove('show-error');
-    this.login();
+    if (validFields) {
+      this.login();
+    }
   });
 }
 
