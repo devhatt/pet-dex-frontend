bed5dd5f2f68fe6262228c2d6dd6f427fa2ecdaa
Matheus <mf635097@gmail.com>
2024-06-24 15:21:06 -0300

    feat: add text area component

---
+++ src/components/TextArea/index.js
@@ -0,0 +1,67 @@
+import { Component } from 'pet-dex-utilities';
+import './index.scss';
+
+const events = [
+  'setName',
+  'setPlaceholder',
+  'setMaxLength',
+  'setRequired',
+  'error',
+];
+
+const html = `
+  <div class="textarea-container">
+    <textarea class="textarea-container__textarea" data-select="textarea" rows="1" cols="1"></textarea>
+    <span class="textarea-container__resize-trigger" data-select="resize-trigger" contenteditable></span>
+  </div>
+`;
+
+export default function TextArea({ name, placeholder, maxLength, required }) {
+  Component.call(this, { html, events });
+  const $textarea = this.selected.get('textarea');
+
+  this.setName(name);
+  this.setPlaceholder(placeholder);
+  this.setMaxLength(maxLength);
+  this.setRequired(required);
+
+  $textarea.addEventListener('focus', () =>
+    $textarea.classList.remove('error'),
+  );
+  $textarea.addEventListener('input', () => this.autoResize());
+  window.addEventListener('resize', () => this.autoResize());
+}
+
+TextArea.prototype = Object.assign(TextArea, Component.prototype, {
+  setName(name = '') {
+    this.selected.get('textarea').name = name;
+    this.emit('setName', name);
+  },
+  setPlaceholder(placeholder = '') {
+    this.selected.get('textarea').placeholder = placeholder;
+    this.emit('setPlaceholder', placeholder);
+  },
+  setMaxLength(maxLength) {
+    if (maxLength) {
+      this.selected.get('textarea').maxLength = maxLength;
+    }
+    this.emit('setMaxLength', maxLength);
+  },
+  setRequired(required = false) {
+    this.selected.get('textarea').required = required;
+    this.emit('setRequired', required);
+  },
+  error() {
+    this.selected.get('textarea').classList.add('error');
+    this.emit('error');
+  },
+  autoResize() {
+    const { value } = this.selected.get('textarea');
+
+    this.selected.get('resize-trigger').innerText = value;
+
+    this.selected.get('textarea').style.height = 'auto';
+    this.selected.get('textarea').style.height =
+      `${this.selected.get('resize-trigger').offsetHeight}px`;
+  },
+});
---
+++ src/stories/TextArea.stories.js
@@ -0,0 +1,26 @@
+import Textarea from '../components/TextArea';
+
+export default {
+  title: 'Components/TextArea',
+  render: (args) => {
+    const textarea = new Textarea(args);
+    const $container = document.createElement('div');
+    textarea.mount($container);
+
+    return $container;
+  },
+  argTypes: {
+    name: { control: 'text', default: '' },
+    placeholder: { control: 'text', default: '' },
+    maxLength: { control: 'number', default: '' },
+    required: { control: 'boolean', default: false },
+  },
+};
+
+export const Default = {
+  args: {
+    name: 'textarea',
+    placeholder: 'Escreva o cuidado especial',
+    required: true,
+  },
+};
