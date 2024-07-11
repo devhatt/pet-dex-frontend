918fef980f0a76709adff071c1a1529e8e7c4ddd
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-07-11 17:56:30 -0300

    feat: inserido textarea na PetVetPage

--- src/layouts/app/pages/PetVet/index.js
+++ src/layouts/app/pages/PetVet/index.js
@@ -1,4 +1,5 @@
 import { Component } from 'pet-dex-utilities';
+import TextArea from '../../../../components/TextArea';
 import Button from '../../../../components/Button';
 import Radio from '../../../../components/RadioButton';
 import Vaccine from '../../../../components/Vaccine';
@@ -35,11 +36,12 @@ const html = `
           </div>
           <div class="petvet-page__card-content">
             <p>Cuidados especiais</p>
-            <form >
-            <fieldset data-select="special-care-radio"></fieldset>
+            <form>
+              <fieldset data-select="special-care-radio"></fieldset>
             </form>
           </div>
         </div>
+        <div class="petvet-page__card-text" data-select="special-care-text"></div>
       </div>
     </div>
     <div class="petvet-page__footer" data-select="footer"></div>
@@ -78,19 +80,20 @@ export default function PetVetPage({ vaccines = [] } = {}) {
 
   const $footer = this.selected.get('footer');
   const $specialCareRadio = this.selected.get('special-care-radio');
+  const $specialCareText = this.selected.get('special-care-text');
   const $neuteredRadio = this.selected.get('neutered-radio');
   const $cardGroup = this.selected.get('card-group');
 
   this.vaccine = new Vaccine({ vaccines });
   this.vaccine.mount($cardGroup);
 
-  createAndMount({
+  const notSpecialCare = createAndMount({
     name: 'specialCare',
     text: 'Não',
     mountTo: $specialCareRadio,
     value: 'notSpecialCare',
   });
-  createAndMount({
+  const specialCare = createAndMount({
     name: 'specialCare',
     text: 'Sim',
     mountTo: $specialCareRadio,
@@ -107,6 +110,21 @@ export default function PetVetPage({ vaccines = [] } = {}) {
     text: 'Sim',
     mountTo: $neuteredRadio,
     value: 'neutered',
+  });
+
+  this.specialCareText = new TextArea({
+    name: 'specialCareText',
+    placeholder: 'Escreva o cuidado especial',
+    maxLength: 500,
+    required: true,
+  });
+
+  specialCare.listen('change', () =>
+    this.specialCareText.mount($specialCareText),
+  );
+  notSpecialCare.listen('change', () => {
+    this.specialCareText.selected.get('textarea').value = '';
+    this.specialCareText.unmount($specialCareText);
   });
 
   this.button = new Button({
@@ -128,11 +146,14 @@ export default function PetVetPage({ vaccines = [] } = {}) {
   const emitForm = () => {
     const neuteredValue = document.forms[0].elements.neutered.value;
     const specialCareValue = document.forms[1].elements.specialCare.value;
+    const specialCareText = this.specialCareText.selected.get('textarea').value;
 
     if (!neuteredValue || !specialCareValue) return;
+    if (getBooleanValue(specialCareValue) && !specialCareText) return;
 
     form.isNeutered = getBooleanValue(neuteredValue);
     form.isSpecialCare = getBooleanValue(specialCareValue);
+    form.specialCareText = specialCareText;
 
     form.vaccines = this.vaccine.listVaccines();
     this.emit('submit', form);
