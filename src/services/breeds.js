af1bb71edf22d32f670759b746703e0c872e355e
juliaam <juliademoraess@gmail.com>
2024-07-12 18:56:44 -0300

    fix: service breed

--- src/services/breeds.js
+++ src/services/breeds.js
@@ -1,14 +1,18 @@
-const url = 'http://localhost:3000/';
+/* eslint-disable no-console */
+const url = 'http://localhost:3000';
 
-export async function getBreeds() {
-  try {
-    const response = await fetch(`${url}breed`);
+export const BreedsService = {
+  getBreeds: async () => {
+    try {
+      const response = await fetch(`${url}/breed`);
 
-    if (!response.ok) {
-      throw new Error('Ocorreu um erro na requisição');
+      if (!response.ok) {
+        throw new Error('Ocorreu um erro na requisição');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error(`Ocorreu o seguinte erro: ${error}`);
+      return error;
     }
-    return await response.json();
-  } catch (error) {
-    return `Ocorreu o seguinte erro: ${error}`;
-  }
-}
+  },
+};
