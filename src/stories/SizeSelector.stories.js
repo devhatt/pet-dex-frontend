0ab0501ab475c4d7b6fb9d0a9da1ea98841ba538
Jonas Chagas <jonaschagasweb@gmail.com>
2024-07-18 22:53:23 -0300

    feat: finish component 2.0

--- src/stories/SizeSelector.stories.js
+++ src/stories/SizeSelector.stories.js
@@ -2,12 +2,9 @@ import SizeSelector from '../components/SizeSelector';
 
 export default {
   title: 'Components/SizeSelector',
-  render: (args) => {
+  render: () => {
     const sizeselector = new SizeSelector();
-    sizeselector.selectCard(
-      sizeselector.$cards[args.selectCard],
-      args.selectCard,
-    );
+
     const $container = document.createElement('div');
     $container.style.width = '100%';
     $container.style.display = 'flex';
@@ -16,29 +13,6 @@ export default {
 
     return $container;
   },
-  argTypes: {
-    card: { control: 'number', default: 1 },
-    selectCard: { control: 'number', default: 1 },
-  },
 };
 
-export const Default = {
-  args: {
-    card: 1,
-    selectCard: 1,
-  },
-};
-
-export const CardText = {
-  args: {
-    ...Default.args,
-    card: 1,
-  },
-};
-
-export const CardSelect = {
-  args: {
-    ...Default.args,
-    selectCard: 1,
-  },
-};
+export const Default = {};
