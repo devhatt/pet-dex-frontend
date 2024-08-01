5bc0720df82297d15f26d278edeea9dbc5f7b994
DominMFD <mfdmatheus@hotmail.com>
2024-08-01 09:43:57 -0300

    fix: month selector array

--- src/components/Calendar/components/DateSelectorComposer/utils/arraysGenerators.js
+++ src/components/Calendar/components/DateSelectorComposer/utils/arraysGenerators.js
@@ -1,7 +1,7 @@
 import { MONTHS } from '../../../utils/months';
 
 export function monthArrayGenerator(month) {
-  const monthArray = new Array(7);
+  const monthArray = new Array(103);
   for (let i = 0; i < monthArray.length; i += 1) {
     const monthIndex = (month - (3 - i) + 12) % 12;
     monthArray[i] = MONTHS[monthIndex];
