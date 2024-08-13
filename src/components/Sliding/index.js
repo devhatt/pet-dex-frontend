ce97627f130734d1c1111719e485b4a0d988688e
Matheus <mf635097@gmail.com>
2024-08-13 12:39:05 -0300

    refactor: adjust component event name to follow project standards

--- src/components/Sliding/index.js
+++ src/components/Sliding/index.js
@@ -9,7 +9,7 @@ const events = [
   'slide:remove',
   'slides:clear',
   'slides:mode',
-  'padding',
+  'padding:change',
 ];
 
 const activeSlide = (slides, slide) => {
@@ -174,7 +174,7 @@ Sliding.prototype = Object.assign(Sliding.prototype, Component.prototype, {
   setSidePadding(sidePadding = 0) {
     this.selected.get('sliding-content').style.padding = `0 ${sidePadding}px`;
 
-    this.emit('padding', sidePadding);
+    this.emit('padding:change', sidePadding);
   },
 
   setShuffleMode(shuffleMode = false) {
