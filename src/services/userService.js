a538cf6c39bcb3236e70a3fb5facf550406b6d25
João Burgarell <jp.burgarelli@gmail.com>
2024-08-19 22:02:40 -0300

    feat: add user registration service (#293)
    * feat: add user registration service
    
    * chore: transform userRegister file into a userService method

--- src/services/userService.js
+++ src/services/userService.js
@@ -16,6 +16,7 @@ export const UserService = {
       return `An error occurred: ${error}`;
     }
   },
+
   login: async (email, password) => {
     try {
       const response = await fetch(`${url}/api/user/login`, {
@@ -29,6 +30,34 @@ export const UserService = {
       return {
         success: true,
         token,
+      };
+    } catch (error) {
+      console.error(`An error occurred: ${error}`);
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  },
+
+  registerUser: async (userData) => {
+    try {
+      const response = await fetch(`${url}/naoSeiARota`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to register user');
+      }
+
+      const result = await response.json();
+      return {
+        success: true,
+        data: result,
       };
     } catch (error) {
       console.error(`An error occurred: ${error}`);
